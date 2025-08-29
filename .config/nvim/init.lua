838164706f7391c14251b6f7e68a8f4bbae8a579
bartek <wlodarski_bartosz@o2.pl>
2025-08-29 22:00:27 +0200

    update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,74 @@
+-- Source existing vimrc for compatibility
 vim.cmd("source ~/.vimrc")
+
+-- Create Explore command
 vim.api.nvim_create_user_command("E", "Explore", {})
+
+-- Bootstrap lazy.nvim plugin manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Plugin setup
+require("lazy").setup({
+  -- Telescope fuzzy finder
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require('telescope').setup{
+        defaults = {
+          -- Default configuration for telescope goes here:
+          mappings = {
+            i = {
+              -- map actions.which_key to <C-h> (default: <C-/>)
+              ["<C-h>"] = "which_key"
+            }
+          }
+        },
+        pickers = {
+          -- Default configuration for builtin pickers goes here:
+          find_files = {
+            theme = "dropdown",
+          }
+        },
+        extensions = {
+          -- Your extension configuration goes here:
+        }
+      }
+    end
+  },
+
+  -- Optional: Better performance for telescope
+  {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    build = 'make',
+    config = function()
+      require('telescope').load_extension('fzf')
+    end
+  },
+})
+
+-- Telescope keymaps
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+vim.keymap.set('n', '<leader>fr', builtin.oldfiles, { desc = 'Telescope recent files' })
+vim.keymap.set('n', '<leader>fc', builtin.commands, { desc = 'Telescope commands' })
+vim.keymap.set('n', '<leader>fs', builtin.grep_string, { desc = 'Telescope grep string under cursor' })
+
+-- Set leader key (if not already set in vimrc)
+vim.g.mapleader = " "
 
